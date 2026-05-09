dc95128e707a07140d23b4b9615f5d28c516fefd
Mawadda <moodalshukaili99@gmail.com>
2026-05-09 12:02:54 +0400

    initial constructor

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,28 @@
-public class Student {
+public class Student extends Person {
+    int StudyLevel;
+    String specialization;
+    double GPA;
+
+    public Student(int studyLevel, String specialization, double GPA) {
+        StudyLevel = studyLevel;
+        this.specialization = specialization;
+        this.GPA = GPA;
+    }
+
+    public Student(String name, Double age, String address, String nationality, int studyLevel, String specialization, double GPA) {
+        super(name, age, address, nationality);
+        StudyLevel = studyLevel;
+        this.specialization = specialization;
+        this.GPA = GPA;
+    }
+
+
+    public static void main(String[] args){
+
+        Student student1 = new Student();
+
+
+    }
+
 }
+
