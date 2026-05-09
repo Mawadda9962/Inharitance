01196c4fb3f89644bb2b93d9855b449f6b310e80
Mawadda <moodalshukaili99@gmail.com>
2026-05-09 11:52:05 +0400

    Creating getter and setter for the attributes

--- src/Person.java
+++ src/Person.java
@@ -14,4 +14,36 @@ public class Person {
         this.address = address;
         this.nationality = nationality;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAge() {
+        return age;
+    }
+
+    public void setAge(Double age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 }
