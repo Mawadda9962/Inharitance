ee274f1506b3487adc2ad4e4723a098e9129b38f
Mawadda <moodalshukaili99@gmail.com>
2026-05-09 17:29:39 +0400

    Adding override

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Student extends Person {
     int StudyLevel;
     String specialization;
     double GPA;
 
-    public Student(int studyLevel, String specialization, double GPA) {
+    Scanner sr = new Scanner(System.in);
+
+    public Student() {
+        int studyLevel;
         StudyLevel = studyLevel;
         this.specialization = specialization;
         this.GPA = GPA;
@@ -19,7 +24,12 @@ public class Student extends Person {
 
     public static void main(String[] args){
 
+       // Student student1 = new Student("Ahmed", 21.3, "Muscat", "Omani", 2, "IT", 3.44);
+
         Student student1 = new Student();
+        student1
+
+        System.out.println(student1.getName());
 
 
     }
