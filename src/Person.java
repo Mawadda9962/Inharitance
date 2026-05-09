a6e4e3fdd8cc76194923d5f1012e98198ff2b441
Mawadda <moodalshukaili99@gmail.com>
2026-05-09 11:49:48 +0400

    Creating a new class

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    String name;
+    Double age;
+    String address;
+    String nationality;
+
+    public Person(){
+
+    }
+
+    public Person(String name, Double age, String address, String nationality) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.nationality = nationality;
+    }
+}
